0514af54afb996a0b469fa61d102cc8f0ca8a509
Grey Ruessler <greyruessler+git@gmail.com>
2024-08-03 21:25:58 -0500

    config mock / setup

---
+++ pages/greycodes.net/src/app/core/services/config.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+
+//TODO: runtime config
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+  constructor() {}
+}
