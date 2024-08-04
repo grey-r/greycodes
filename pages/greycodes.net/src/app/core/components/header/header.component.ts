8f02045fc465260ca5173c5ed542888dbc8d05bd
Grey Ruessler <greyruessler+git@gmail.com>
2024-08-04 13:28:08 -0500

    back to fontawesome svg!!!

--- pages/greycodes.net/src/app/core/components/header/header.component.ts
+++ pages/greycodes.net/src/app/core/components/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faInstagram,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  public readonly faBars = faBars;
+  public readonly faLinkedinIn = faLinkedinIn;
+  public readonly faInstagram = faInstagram;
+  public readonly faGithub = faGithub;
 }
